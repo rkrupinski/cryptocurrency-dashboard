394fe73f6efa180734c77ee9e6311585134e9b95
Rafał Krupiński <krupinski.rafal@gmail.com>
2017-12-27 16:26:24 +0100

    Don’t even try to refactor it.

--- src/components/currencyPicker/connected.ts
+++ src/components/currencyPicker/connected.ts
@@ -1,12 +1,21 @@
 import { connect } from 'react-redux';
+import { branch, renderComponent, compose } from 'recompose';
 
-import { CurrencyPicker } from '.';
+import { CurrencyPicker, ICurrencyPickerProps } from '.';
+import { Spinner } from '@src/components/spinner';
 import { IRootState } from '@src/redux_';
 
 const mapStateToProps = ({ currencies }: IRootState) => ({
   ...currencies,
 });
 
-export const CurrencyPickerConnected = connect(
-  mapStateToProps,
+export const CurrencyPickerConnected = compose<ICurrencyPickerProps, {}>(
+  connect(
+    mapStateToProps,
+  ),
+  branch<ICurrencyPickerProps>(
+    ({ loading }) => loading,
+    renderComponent(Spinner),
+    renderComponent(CurrencyPicker),
+  ),
 )(CurrencyPicker);
--- src/components/currencyPicker/currencyPicker.tsx
+++ src/components/currencyPicker/currencyPicker.tsx
@@ -1,24 +1,15 @@
 import React, { StatelessComponent } from 'react';
-import { branch, renderNothing, renderComponent } from 'recompose';
-import { CircularProgress } from 'material-ui/Progress';
 
 import { Currency } from '@src/redux_/currencies';
-import { Spinner } from '@src/components/spinner';
 
-interface ICurrencyPickerProps {
+export interface ICurrencyPickerProps {
   all: Currency[];
   loading: boolean;
   selected: string[];
 }
 
-const Foo: StatelessComponent<ICurrencyPickerProps> = ({
+export const CurrencyPicker: StatelessComponent<ICurrencyPickerProps> = ({
   all,
 }) => (
   <div>Currencies: {all.length}</div>
 );
-
-export const CurrencyPicker = branch<ICurrencyPickerProps>(
-  ({ loading }) => loading,
-  renderComponent(Spinner),
-  renderComponent(Foo),
-)(Foo);
